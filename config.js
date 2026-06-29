cb12155e8461b90a849385eb4b9373586f53e521
aria0929 <jiean0929@gmail.com>
2026-06-29 15:42:26 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// ⚠️ 本檔案包含您的 Firebase 雲端資料庫私密金鑰，已設定 .gitignore 忽略，不會上傳到 GitHub 進行公開。
+const firebaseConfig = {
+    apiKey: "AIzaSyAwtFU4H427t3C6cJtkdVNEEmt55cb9W1Y",
+    authDomain: "cyber-snake-bee31.firebaseapp.com",
+    databaseURL: "https://cyber-snake-bee31-default-rtdb.firebaseio.com",
+    projectId: "cyber-snake-bee31",
+    storageBucket: "cyber-snake-bee31.firebasestorage.app",
+    messagingSenderId: "141518279757",
+    appId: "1:141518279757:web:c6c4330482062e50043804",
+    measurementId: "G-5YRXT80BMC"
+};
